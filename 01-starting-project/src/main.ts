3e865f245cab9224ce437ff5ae9c729ef31cabb8
V <V@DESKTOP>
2024-08-26 20:14:42 +0300

    first commit

---
+++ 01-starting-project/src/main.ts
@@ -0,0 +1,9 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+import { HeaderComponent } from './app/header/header.component';
+import { UserComponent } from './app/user/user.component';
+
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(HeaderComponent);
+bootstrapApplication(UserComponent);
